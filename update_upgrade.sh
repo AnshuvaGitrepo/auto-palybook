0366a255d840a9ecde702f8830185d8ca3516cee
Anshu <137651402+AnshuvaGitrepo@users.noreply.github.com>
2025-08-13 22:05:08 +0530

    Update update_upgrade.sh

--- update_upgrade.sh
+++ update_upgrade.sh
@@ -7,8 +7,8 @@ echo "Uninstalling Fastfetch..."
 if command -v fastfetch &>/dev/null; then
     if dpkg -l | grep -q fastfetch; then
         echo "Removing Fastfetch (apt)..."
-        sudo apt-get remove --purge -y fastfetch
-        sudo apt-get autoremove -y
+        apt-get remove --purge -y fastfetch
+        apt-get autoremove -y
     else
         echo "Fastfetch binary found but not an apt package."
     fi
@@ -19,7 +19,7 @@ fi
 # If installed from source in /usr/local
 if [ -f "/usr/local/bin/fastfetch" ]; then
     echo "Removing Fastfetch binary from /usr/local..."
-    sudo rm -f /usr/local/bin/fastfetch
+    rm -f /usr/local/bin/fastfetch
 fi
 
 # If installed in user home dir
